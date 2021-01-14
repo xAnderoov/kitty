4afa972818380e217c099fd9398ac603b8c59746
xAnderoov <psashv@gmail.com>
2021-01-14 08:44:26 +0300

    setup mobile menu

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import "./scss/app.scss";
 
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const open = document.querySelector(".header__menu--open");
 const close = document.querySelector(".header__menu--close");
 
 open.addEventListener("click", () => {
   header.classList.add("header--fullscreen");
+  body.classList.add("overflow-hidden");
 });
 
 close.addEventListener("click", () => {
   header.classList.remove("header--fullscreen");
+  body.classList.remove("overflow-hidden");
 });
