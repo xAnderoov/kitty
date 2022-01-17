72dd4d82bc686536ab80de6af7979e75c298158a
xAnderoov <psashv@gmail.com>
2022-01-17 18:33:51 +0300

    g updating packages

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,46 @@ const path = require("path");
 const miniCss = require("mini-css-extract-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(s*)css$/,
-        use: [miniCss.loader, "css-loader?url=false", "sass-loader"],
-      },
-    ],
+    assetModuleFilename: "[name][ext]",
   },
   plugins: [
     new miniCss({
       filename: "style.css",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: [
+          miniCss.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "[name][ext]",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: ["extract-loader", "html-loader"],
+      },
+    ],
+  },
 };
+
+// find complete solution
+// copy html file with pasing paths
