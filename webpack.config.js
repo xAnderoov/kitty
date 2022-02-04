615d00c1ca472a0fcb9621fcf9f32dbb27e77c7c
xAnderoov <psashv@gmail.com>
2022-02-04 06:38:48 +0300

    setup c(s)ss loaders

--- webpack.config.js
+++ webpack.config.js
@@ -7,41 +7,35 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: "[name][ext]",
   },
-  plugins: [
-    new miniCss({
-      filename: "style.css",
-    }),
-  ],
   module: {
     rules: [
       {
         test: /\.(s*)css$/,
         use: [
           miniCss.loader,
-          "css-loader",
-          "sass-loader",
-        ],
+          {
+            loader: "css-loader",
+            options: {
+              url: false,
+              sourceMap: true
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+        ]
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.html$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "[name][ext]",
-        },
-      },
-      {
-        test: /\.html$/,
-        use: ["extract-loader", "html-loader"],
-      },
-    ],
+    ]
   },
+  plugins: [
+    new miniCss({
+      filename: "style.css",
+    }),
+  ]
 };
 
-// find complete solution
-// copy html file with pasing paths
+// sourceMap make it work
